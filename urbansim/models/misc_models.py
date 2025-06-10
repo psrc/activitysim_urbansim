167fbdea7a9e4e3ca1b476be9e6e7a5b10e01521
Hana Sevcikova <hsevcikova@psrc.org>
2025-06-10 16:30:48 -0700

    model for adding households

--- urbansim/models/misc_models.py
+++ urbansim/models/misc_models.py
@@ -2,6 +2,7 @@ from activitysim.core import workflow
 from activitysim.core import expressions
 from activitysim.core.configuration import PydanticBase
 import pandas as pd
+import numpy as np
 
 @workflow.step()
 def empty_model(state: workflow.State) -> None:
@@ -25,3 +26,15 @@ def compute_variables_for_households(state: workflow.State,
     print("Average number of children of age < 18 = ", households["num_children"].mean())
     print("Average number of children of age < 13 = ", households["persons_under_13"].mean())
     return None
+
+@workflow.step()
+def add_new_households(state: workflow.State,
+                       households: pd.DataFrame) -> None:
+    n = 10
+    sample = households.sample(n)
+    # change the index, i.e. household_id
+    sample.index = np.arange(households.index.max() + 1, households.index.max() + n + 1)
+    # concatenate
+    households = pd.concat([households, sample])
+    return None
+
