ee929c1f2f8e65fdb4e61a2d8f5a1b31feeea691
Hana Sevcikova <hsevcikova@psrc.org>
2025-06-10 15:02:18 -0700

    code updated to latest activitysim

--- simulate.py
+++ simulate.py
@@ -1,43 +1,20 @@
-# Urbansim via ActivitySim
+# Run simple models via the ActivitySim pipeline
+
+## Installation of python for activitysim
+## following https://activitysim.github.io/activitysim/v1.3.1/users-guide/modelsetup.html
+## Needed to downgrade the numba package
+# ########################################
+# mamba create -n asim python=3.10 activitysim -c conda-forge --override-channels
+# conda activate asim
+# pip install numba==0.58.1
 
 import argparse
 import sys
-#import logging
-#from activitysim.core import inject
-#from activitysim.core import config
-##from activitysim.core import tracing
-#from activitysim.core import pipeline
-
-#from activitysim.core.config import handle_standard_args
-#from activitysim.core.tracing import print_elapsed_time
-
-
+from activitysim import abm
 from activitysim.cli.run import add_run_args, run
-
-#from activitysim import abm
 import urbansim.models
 
 if __name__ == "__main__":
-
-    #handle_standard_args()
-    
-    ##tracing.config_logger()
-    
-    #t0 = print_elapsed_time()
-    
-    #logger = logging.getLogger('urbansim') 
-    
-    #settings = inject.get_injectable("settings")
-    
-    #models = settings.get("models")
-    
-    #pipeline.run(models=models)
-    
-    #pipeline.close_pipeline()
-    
-    #t0 = ("all models", t0)    
-
-
     parser = argparse.ArgumentParser()
     add_run_args(parser)
     args = parser.parse_args()
--- urbansim/models/misc_models.py
+++ urbansim/models/misc_models.py
@@ -1,17 +1,27 @@
-from activitysim.core import inject
+from activitysim.core import workflow
 from activitysim.core import expressions
-    
-@inject.step()
-def empty_model():
+from activitysim.core.configuration import PydanticBase
+import pandas as pd
+
+@workflow.step()
+def empty_model(state: workflow.State) -> None:
     print("This is an empty model")
-    return
+    return None
 
-@inject.step()
-def count_households(households):
+@workflow.step()
+def count_households(state: workflow.State,
+                     households: pd.DataFrame) -> None:
     print("Number of households ", len(households))
-    return
+    return None
 
-@inject.step()
-def compute_variables_for_households(households, settings):
-    expressions.assign_columns(households, model_settings=settings.get("household_variables"))
-    return
+@workflow.step()
+def compute_variables_for_households(state: workflow.State,
+                                     households: pd.DataFrame,
+                                     settings: dict | PydanticBase) -> None:
+    # Compute a bunch of variables that also require the persons dataset
+    if isinstance(settings, PydanticBase):
+        settings = settings.dict()    
+    expressions.assign_columns(state, households, model_settings=settings.get("household_variables"))
+    print("Average number of children of age < 18 = ", households["num_children"].mean())
+    print("Average number of children of age < 13 = ", households["persons_under_13"].mean())
+    return None
