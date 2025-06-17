bf1fceedd16efed4f9f2949e9709a4acf7be9937
Modeller <coestefan@gmail.com>
2025-06-17 11:55:02 -0700

    Write out tables at end of simulation year.

--- urbansim/models/misc_models.py
+++ urbansim/models/misc_models.py
@@ -3,6 +3,8 @@ from activitysim.core import expressions
 from activitysim.core.configuration import PydanticBase
 import pandas as pd
 import numpy as np
+import pathlib
+import os
 
 @workflow.step()
 def empty_model(state: workflow.State) -> None:
@@ -49,6 +51,34 @@ def remove_households(state: workflow.State,
     households = households.drop(sample.index)
     state.add_table("households", households)
     return None
+
+@workflow.step()
+def write_tables_for_sim_year(state: workflow.State,
+                      households: pd.DataFrame, 
+                      persons: pd.DataFrame) -> None:
+    current_year = state.get_injectable("year")
+    if isinstance(state.filesystem.output_dir, pathlib.PurePath): 
+        output_dir = state.filesystem.output_dir/str(current_year)
+    else: 
+        output_dir = state.filesystem.working_dir/f"output/{current_year}"
+
+    if not os.path.exists(output_dir):
+            os.makedirs(output_dir)
+    if current_year == state.get_global_constants()['END_YEAR']:
+        # If this is the last year of the simulation, write out all tables
+        print(f"Writing households and persons tables for year {current_year} to {output_dir}")
+        households.to_parquet(output_dir/"households.parquet", compression="gzip")
+        persons.to_parquet(output_dir/"persons.parquet", compression="gzip")
+        print(f"Tables households and persons written to {output_dir}")
+
+    else:
+        # If this is not the last year of the simulation, write out only the persons and households table
+        print(f"Writing households table for year {current_year} to {output_dir}")
+        households.to_parquet(output_dir/"households.parquet", compression="gzip")
+        persons.to_parquet(output_dir/"persons.parquet", compression="gzip")
+        print(f"Table households written to {output_dir}")
+    
+
 
 # @workflow.step()
 # def add_column_to_households(state: workflow.State,
